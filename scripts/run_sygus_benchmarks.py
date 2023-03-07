09bd6c717228630fd068c8686d95b9134fe9d056
Sankha Narayan Guria <sankha93@gmail.com>
2023-03-07 15:30:59 -0500

    json -> csv

--- scripts/run_sygus_benchmarks.py
+++ scripts/run_sygus_benchmarks.py
@@ -6,6 +6,7 @@ from scipy.stats import iqr
 import os
 import argparse
 import sys
+import csv
 
 parser = argparse.ArgumentParser(description='Run RbSyn benchmarks')
 parser.add_argument('--times', '-t', dest='times', action='store',
@@ -50,6 +51,34 @@ def collect(output_file, times, **opts):
     with open(output_file, 'w') as out:
         json.dump(merged, out)
 
+def combine_results(base, no_template, no_cache):
+    for k, v in no_template.items():
+        if k not in base:
+            base[k] = {}
+        base[k]['no_template'] = v['time'][0]
+    for k, v in no_cache.items():
+        if k not in base:
+            base[k] = {}
+        base[k]['no_cache'] = v['time'][0]
+    return base
+
+def to_table(data, filename):
+    with open(filename, 'w', newline='') as csvfile:
+        tablewriter = csv.writer(csvfile)
+        tablewriter.writerow(['Name', 'Time Median (s)', 'Time SIQR (s)', 'Size', '# Ex', 'Tested Progs', 'No cache', 'No template'])
+        for k, v in data.items():
+            tablewriter.writerow([k, v['median_time'], v['time_siqr'], v['size'], v['specs'], v['tested_progs'], v['no_cache'], v['no_template']])
+
 collect('sygus_data.json', int(args.times))
 collect('sygus_template_infer.json', 1, TEMPLATE_INFER='1')
 collect('sygus_no_cache.json', 1, NO_CACHE='1')
+
+with open('sygus_data.json', 'r') as f:
+    base = json.load(f)
+with open('sygus_template_infer.json', 'r') as f:
+    no_template = json.load(f)
+with open('sygus_no_cache.json', 'r') as f:
+    no_cache = json.load(f)
+
+data = combine_results(base, no_template, no_cache)
+to_table(data, 'table1.csv')
